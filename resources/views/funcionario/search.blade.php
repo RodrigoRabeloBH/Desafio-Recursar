08eeb0c7ff55f7f6bd5412f5932b001e60069026
Rodrigo Rabelo <rodrigoalexandrerabelo@gmail.com>
2020-06-28 01:40:41 -0300

    Ajuste search view

--- resources/views/funcionario/search.blade.php
+++ resources/views/funcionario/search.blade.php
@@ -9,21 +9,19 @@
             <table class="table table-hover">
                 <thead class="thead-light">               
                     <th>Nome</th>
-                    <th>Data de Nascimento</th>
-                    <th>Salário</th>               
+                    <th>Data de Nascimento</th>                        
                     <th></th>
                 </thead>
                 <tbody id="tbody">                    
                         @foreach ($funcionarios as $funcionario)
                         <tr>                       
                             <td>{{$funcionario->Nome}}</td>              
-                            <td>{{date('d/m/Y', strtotime($funcionario->DataNascimento))}}</td>              
-                            <td>{{$funcionario->Salario}}</td>      
-                            <td class="text-right">
+                            <td>{{date('d/m/Y', strtotime($funcionario->DataNascimento))}}</td>        
+                             <td class="text-right">
                                 <a href="/funcionario/{{$funcionario->CodFuncionario}}" class="btn-sm btn-secondary mx-1">
-                                    <i class="fas fa-info-circle"></i>
+                                    <i class="fas fa-info-circle my-2"></i>
                                 </a>
-                                <a href="/funcionario/{{$funcionario->CodFuncionario}}/edit" class="btn-sm btn-info my-2 mx-1">
+                                <a href="/funcionario/{{$funcionario->CodFuncionario}}/edit" class="btn-sm btn-info mx-1">
                                     <i class="fas fa-pencil-alt"></i>
                                 </a>                                   
                             </td>              
